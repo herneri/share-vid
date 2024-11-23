a423ad6dac449f29211f19081f74a30729b71f98
Eric Hernandez <herneri@protonmail.com>
2024-11-23 12:04:25 -0600

    feat: Make setup script more versatile

--- setup.sh
+++ setup.sh
@@ -15,23 +15,64 @@
 #	See the License for the specific language governing permissions and
 #	limitations under the License.
 
-if [ "$1" = "-h" ] || [ ! $1 ]; then
-	printf "usage: setup.sh [VIDEO_DIRECTORY] \n"
-	exit 0
-fi
+dependency_check() {
+	local dependencies="python3 flask mysql httpd"
 
-dependencies="python3 flask mysql httpd"
-for dependency in $dependencies
-do
-	if ! which $dependency >> /dev/null; then
-		printf "ERROR: $dependency is not installed \n"
+	for dependency in $dependencies
+	do
+		if ! which $dependency >> /dev/null; then
+			printf "ERROR: $dependency is not installed \n"
+			exit 1
+		fi
+	done
+
+	return
+}
+
+import_videos() {
+	if [ ! $1 ]; then
+		printf "ERROR: Missing video directory \n"
 		exit 1
 	fi
-done
 
-mkdir static/videos
-cp $1/* static/videos/
+	mkdir static/videos
+	cp $1/* static/videos/
 
-echo "CREATE DATABASE sharevid" | mysql
-mysql sharevid < sql/create-tables.sql
-echo "y" | ./sv-mgmt.py -u -v
+	return
+}
+
+create_database() {
+	echo "CREATE DATABASE sharevid" | mysql
+	mysql sharevid < sql/create-tables.sql
+	echo "y" | ./sv-mgmt.py -u -v
+	return
+}
+
+case "$1" in
+	"-d")
+		dependency_check
+	;;
+
+	"-i")
+		import_videos $2
+	;;
+
+	"-c")
+		create_database
+	;;
+
+	"-a")
+		dependency_check
+		import_videos $2
+		create_database
+	;;
+
+	*)
+		echo -e "usage: setup.sh [OPTION] [VIDEO_DIRECTORY]"
+		echo -e "-a [VIDEO_DIRECTORY]\tDo all setup operations listed below"
+		echo -e "-d\t\t\tCheck if dependencies are present"
+		echo -e "-i [VIDEO_DIRECTORY]\tImport videos"
+		echo -e "-c\t\t\tCreate ShareVid database"
+		exit 0
+	;;
+esac
